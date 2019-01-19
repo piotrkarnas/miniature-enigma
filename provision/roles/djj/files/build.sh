69dae88eb1478f5fe79e567c9d16178738174e66
Piotr Karnas <piotr@karnas.com.pl>
2019-01-19 13:05:24 +0100

    fake update

--- provision/roles/djj/files/build.sh
+++ provision/roles/djj/files/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 jenkinssecret=$(docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
-java -jar /root/jenkins-cli.jar -s http://localhost:8080 -auth admin:$jenkinssecret build lasttestbuild
+java -jar /root/jenkins-cli.jar -s http://127.0.0.1:8080 -auth admin:$jenkinssecret build lasttestbuild
 sleep 300
