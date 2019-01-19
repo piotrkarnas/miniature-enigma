9d2968f79b7cf857f030886ae7df4d80b8bac79d
Piotr Karnas <piotr@karnas.com.pl>
2019-01-19 13:43:44 +0100

    forgot the loop

--- provision/roles/djj/files/build.sh
+++ provision/roles/djj/files/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 jenkinssecret=$(docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword)
-java -jar /root/jenkins-cli.jar -s http://127.0.0.1:8080 -auth admin:$jenkinssecret build lasttestbuild
-sleep 300
+while (true); do
+	java -jar /root/jenkins-cli.jar -s http://127.0.0.1:8080 -auth admin:$jenkinssecret build lasttestbuild
+	sleep 300
+done
